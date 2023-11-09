a0932360b872c988f4740d1be7fdbd7ca4f08aa4
youtkddnjs <youtkddnjs91@gmail.com>
2023-11-09 11:42:35 +0900

    add firebase environment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,4 +49,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    //firebase 실시간 데이터
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    //com.google.firebase:firebase-messaging-ktx	23.2.1
+    implementation("com.google.firebase:firebase-messaging-ktx")
 }
--- app/src/main/java/mhha/sample/mychahting/MainActivity.kt
+++ app/src/main/java/mhha/sample/mychahting/MainActivity.kt
@@ -2,10 +2,16 @@ package mhha.sample.mychahting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mhha.sample.mychahting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    } //override fun onCreate(savedInstanceState: Bundle?)
+} //class MainActivity : AppCompatActivity()
